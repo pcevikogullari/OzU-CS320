e8e69093401cceae5be6c6ad4f40815784c46dcd
turushanaktay <turushan.aktay@ozu.edu.tr>
2014-04-12 21:15:53 +0300

    iOS v0.1
    iOS GUI draft

---
+++ ios/Cetin/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Cetin
+//
+//  Created by Turushan Aktay on 09/04/14.
+//  Copyright (c) 2014 Turushan Aktay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+- (IBAction)loginpush:(id)sender {
+    NSLog(@"hello");
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
