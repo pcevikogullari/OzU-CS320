29353448edad3e9ec90548326dd9318532ce408e
Oguz Gelal <o.gelal77@gmail.com>
2014-04-14 20:33:52 +0300

    added backend codes

---
+++ Backend_Web/system/classes/listener.php
@@ -0,0 +1,8 @@
+<?php
+class listener {
+	
+	public function retrieve(){
+		
+	}
+
+}
---
+++ Backend_Web/system/functions/functions.php
@@ -0,0 +1,18 @@
+<?php
+function request($url){
+	$session_name = session_name();
+	$session_id = session_id();
+	session_write_close();
+	// init the request
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl, CURLOPT_COOKIE, $session_name.'='.$session_id);
+	$res = curl_exec($curl);
+	curl_close($curl);
+	// start session
+	@session_id($session_id);
+	@session_start();
+	return $res;
+}
+?>
